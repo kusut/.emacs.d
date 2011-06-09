ce593488c8b91c07905aa5d6e123a614d8837ec1
kusut <kusut@dahsy.at>
2011-06-09 17:40:30 +0700

    new autopair. autocomplete. rope. and shit

--- init.el
+++ init.el
@@ -1,8 +1,28 @@
+(let ((default-dir "~/.emacs.d/vendor/"))
+  (add-to-list 'load-path default-dir)
+  (normal-top-level-add-subdirs-to-load-path))
+
+(setq backup-inhibited t)
+(setq auto-save-default nil)
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/vendor/auto-complete/ac-dict")
+(ac-config-default)
+
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
+
+
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
 (setq-default truncate-lines t)
 (global-set-key [f12] 'toggle-truncate-lines)
+
+
+(require 'autopair)
+(autopair-global-mode)
 
 ;;; code checking. requires pyflakes
 (when (load "flymake" t) 
@@ -19,16 +39,3 @@
 
 (add-hook 'find-file-hook 'flymake-find-file-hook)
 ;;; end code checking
-
-;;; pair
-(add-hook 'python-mode-hook
-	  (lambda ()
-	    (define-key python-mode-map "\"" 'pair)
-	    (define-key python-mode-map "\'" 'pair)
-	    (define-key python-mode-map "(" 'pair)
-	    (define-key python-mode-map "[" 'pair)
-	    (define-key python-mode-map "{" 'pair)))
-(defun pair ()
-  (interactive)
-  (let (parens-require-spaces)
-    (insert-pair)))
