56d796fd89332d5b4eaed978990aed27de757d87
kusut <none@none>
2012-04-01 18:33:17 +0700

    tinja ninja ninja

--- modes.el
+++ modes.el
@@ -1,2 +1,2 @@
-(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mumamo-mode))
+(add-to-list 'auto-mode-alist '("\\.jinja2*$" . django-html-mumamo-mode))
 (add-to-list 'auto-mode-alist '("\\.mako*$" . mako-html-mumamo-mode))
