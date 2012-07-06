7301407088fb892b47182551c368e90129b9af8f
kusut <kusut@dahsy.at>
2012-07-06 17:12:04 +0700

    markdown

--- modes.el
+++ modes.el
@@ -1,2 +1,3 @@
 (add-to-list 'auto-mode-alist '("\\.jinja2*$" . django-html-mumamo-mode))
 (add-to-list 'auto-mode-alist '("\\.mako*$" . mako-html-mumamo-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
