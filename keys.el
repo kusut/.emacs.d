c0e2fa6bb2603be80f5bebf07994bf13cb2122ef
kusut <kusut@dahsy.at>
2012-07-10 18:26:53 +0700

    remove mako key binding

--- keys.el
+++ keys.el
@@ -1,14 +1,10 @@
 (global-set-key [f12] 'toggle-truncate-lines)
+(global-set-key [f11] 'django-nxhtml-mumamo-mode)
 
-;require nxhtml
-(global-set-key [f8] 'django-nxhtml-mumamo-mode)
-(global-set-key [f7] 'mako-nxhtml-mumamo-mode)
-
-;require flymake setup
 (global-set-key [f5] 'flymake-goto-prev-error)
 (global-set-key [f6] 'flymake-goto-next-error)
 
 (global-set-key (kbd "M-t") 'pdb-trace)
 (global-set-key (kbd "C-w") 'kill-region-or-backward-kill-word)
 (global-set-key (kbd "C-c i") 'magit-status)
-(global-set-key (kbd "C-c C-i") 'magit-status)
+
