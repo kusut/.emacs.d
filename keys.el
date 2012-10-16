edb94b52af642587550f7d9fa96336eb18265f68
kusut <tinokusut@gmail.com>
2012-10-16 15:14:06 +0700

    copy-region-or-copy-line

--- keys.el
+++ keys.el
@@ -3,13 +3,21 @@
 (defun toggle-sgml-indent ()
   (interactive)
   (message "sgml basic offset = %s"
-            (setq sgml-basic-offset (if (= sgml-basic-offset 2) 4 2)) ))
+           (setq sgml-basic-offset (if (= sgml-basic-offset 2) 4 2)) ))
 
 (defun kill-region-or-backward-kill-word (&optional arg region)
   (interactive (list (prefix-numeric-value current-prefix-arg) (use-region-p)))
   (if region 
       (kill-region (region-beginning) (region-end))
     (backward-kill-word arg)))
+
+(defun copy-region-or-copy-line (&optional arg region)
+  (interactive (list (prefix-numeric-value current-prefix-arg) (use-region-p)))
+  (if region
+      (kill-ring-save (region-beginning) (region-end))
+    (progn
+      (kill-ring-save (line-beginning-position) (line-beginning-position 2))
+      (message "Line copied"))))
 
 (defun upgrade-packages ()
   (interactive)
@@ -24,6 +32,7 @@
 (global-set-key (kbd "C-c p") 'upgrade-packages)
 (global-set-key (kbd "C-c C-p") 'upgrade-packages)
 (global-set-key (kbd "C-w") 'kill-region-or-backward-kill-word)
+(global-set-key (kbd "M-w") 'copy-region-or-copy-line)
 (global-set-key (kbd "M-t") 'pdb-trace)
 
 (global-set-key (kbd "C-=") 'er/expand-region)
