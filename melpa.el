c3ceb54047b90718d2deae8691fb4409549f28cb
kusut <tinokusut@gmail.com>
2015-11-18 22:22:54 +0700

    remove mmm-mako, autocomplete, and haskell stuff for now

--- melpa.el
+++ melpa.el
@@ -8,18 +8,12 @@
 (defvar packages 
   '(
     flycheck
-    flycheck-haskell
-    ghc
-    ghci-completion
-    haskell-mode
     ido-ubiquitous
     jinja2-mode
     js2-mode
     magit
     markdown-mode
-    mmm-mako
     nginx-mode
-    shm
     zenburn-theme
 ))
 
@@ -37,10 +31,6 @@
 
 (get-packages)
 
-(when (package-installed-p 'auto-complete)
-  (require 'auto-complete-config)
-  (ac-config-default))
-
 (when (package-installed-p 'ido-ubiquitous)
   (require 'ido-ubiquitous)
   (ido-ubiquitous-mode t))
@@ -48,27 +38,10 @@
 (when (package-installed-p 'markdown-mode)
   (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode)))
 
-(when (package-installed-p 'mmm-mako)
-  (require 'mmm-auto)
-  (require 'mmm-mako)
-  (setq mmm-global-mode 'maybe)
-  (add-to-list 'auto-mode-alist '("\\.mako\\'" . html-mode))
-  (mmm-add-mode-ext-class 'html-mode "\\.mako\\'" 'mako))
-
-
 (when (package-installed-p 'zenburn-theme)
   (load-theme 'zenburn t))
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-(add-hook 'haskell-mode-hook 'structured-haskell-mode)
-;(define-key haskell-mode-map (kbd "C-x C-s") 'haskell-mode-save-buffer)
-
-(autoload 'ghc-init "ghc" nil t)
-(add-hook 'haskell-mode-hook
-          (lambda ()
-            (ghc-init)))
-(add-to-list 'exec-path "~/.cabal/bin")
-(setq haskell-stylish-on-save t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (provide 'melpa)
