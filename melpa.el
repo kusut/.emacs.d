f01196354ddb691485a3e000580bd2a1c2dd755c
kusut <tinokusut@gmail.com>
2025-11-10 19:26:02 +0700

    use-package, hotfix seq magit, remove purescript

--- melpa.el
+++ melpa.el
@@ -2,59 +2,53 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'cl)
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; HOTFIX for seq in magit
+(defun seq-keep (function sequence)
+  "Apply FUNCTION to SEQUENCE and return the list of all the non-nil results."
+  (delq nil (seq-map function sequence)))
+
+
+;; repo
+(setq package-archives
+      '(("MELPA Stable" . "https://stable.melpa.org/packages/")
+        ("MELPA"        . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("MELPA Stable" . 1)
+        ("MELPA"        . 4)))
+
+;; setup
 (package-initialize)
-(setq url-http-attempt-keepalives nil)
+(setq-default url-http-attempt-keepalives nil)
+(unless package-archive-contents
+  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+(setq use-package-verbose t)
 
-(defvar packages 
-  '(
-    flycheck
-    groovy-mode
-    haskell-mode
-    ido-completing-read+
-    js2-mode
-    magit
-    markdown-mode
-    nginx-mode
-    purescript-mode
-    zenburn-theme
-))
 
-(defun fully-installed-p ()
-  (loop for p in packages
-        when (not (package-installed-p p)) do (return nil)
-        finally (return t)))
-
-(defun get-packages ()
-  (unless (fully-installed-p)
-    (package-refresh-contents)
-    (dolist (p packages)
-      (unless (package-installed-p p)
-        (package-install p)))))
-
-(get-packages)
-
-(when (package-installed-p 'ido-ubiquitous)
-  (require 'ido-ubiquitous)
-  (ido-ubiquitous-mode t))
-
-(when (package-installed-p 'markdown-mode)
+;; packages
+(use-package flycheck
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+(use-package groovy-mode
+   :config
+   (setq-default groovy-indent-offset 2))
+(use-package haskell-mode)
+(use-package ido-completing-read+ :ensure t :defer 1)
+(use-package js2-mode
+  :config
+  (setq js-indent-level 2)
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+(use-package magit)
+(use-package markdown-mode
+  :config
   (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode)))
+(use-package nginx-mode)
+(use-package zenburn-theme :ensure t)
 
-(when (package-installed-p 'purescript-mode)
-  (require 'purescript-mode)
-  (add-to-list 'auto-mode-alist '("\\.purs$" . purescript-mode))
-  (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation))
-
-(when (package-installed-p 'zenburn-theme)
-  (load-theme 'zenburn t))
-
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(setq js-indent-level 2)
-(setq groovy-indent-offset 2)
 (provide 'melpa)
+;;; melpa.el ends here
